2b965e3cfc306472aeda363ec4481e2833aa9b1b
pjworsham <145511699+pjworsham@users.noreply.github.com>
2025-04-06 12:47:58 -0400

    header footer

--- src/js/pets.js
+++ src/js/pets.js
@@ -1,4 +1,7 @@
+import { loadHeaderFooter } from "./utils.mjs";
+
 import { getAccessToken } from "./utils.mjs";
+
 
 const baseURL = "https://api.petfinder.com/v2/animals"
 
@@ -14,4 +17,59 @@ async function getAllAnimals() {
   console.log(data);
 
 }
-getAllAnimals();
+
+loadHeaderFooter();
+
+
+
+
+
+
+
+
+
+
+
+
+// EXAMPLE OF JS CODE FOR POPULATING DATA FROM PRODUCTDETAILS.MJS
+
+// function productDetailsTemplate(product) {
+//   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+//     <h2 class="divider">${product.NameWithoutBrand}</h2>
+//     <img
+//       class="divider"
+//       src="${product.Images.PrimaryLarge}"
+//       alt="${product.NameWithoutBrand}"
+//     />
+//     <p class="product-card__price">$${product.FinalPrice}</p>
+//     <p class="product__color">${product.Colors[0].ColorName}</p>
+//     <p class="product__description">
+//     ${product.DescriptionHtmlSimple}
+//     </p>
+//     <div class="product-detail__add">
+//       <button id="add-to-cart" data-id="${product.Id}">Add to Cart</button>
+//     </div></section>`;
+// }
+
+// TEMPLATE RENDERING PRODUCT CARDS FROM PRODUCT LIST MJS
+
+// Template for rendering product cards
+// function productCardTemplate(product) {
+//   return `
+//       <li class="product-card">
+//           <a href="product_pages/?product=${product.Id}">
+//               <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}" />
+//               <h3 class="card__brand">${product.Brand.Name}</h3>
+//               <h2 class="card__name">${product.NameWithoutBrand}</h2>
+//               <p class="product-card__price">$${product.FinalPrice}</p>
+//           </a>
+//       </li>
+//   `;
+// }
+
+// SECTION FROM PRODUCT_LISTING HTML GO WITH RENDING PRODUCT CARDS 
+
+// {/* <section class="products">
+// <h2>Top Products: <span class="title highlight"></span></h2>
+// <ul class="product-list"></ul>
+// </section> */}
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,3 +1,7 @@
+let headerLoaded = false; // Variable to know if the header has already been loaded
+let footerLoaded = false; // Variable to know if the footer has already been loaded
+
+
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 const url = 'https://api.petfinder.com/v2/oauth2/token';
@@ -30,3 +34,51 @@ export async function getAccessToken() {
         console.error('Error:', error);  // Handle any errors
     }
 }
+
+
+
+export function renderwithTemplate(template, parentElement, data, callback) {
+
+    // Clear the content before inserting a new template
+    parentElement.innerHTML = "";
+  
+   
+    parentElement.innerHTML = template;
+  
+    if (callback) {
+      callback(data)
+    }
+  
+  }
+  //This asynchronous function fetches the content of the HTML
+  export async function loadTemplate(path) {
+    const res = await fetch(path);
+    //The response is converted to text and returns the HTML content as a string
+    const template = await res.text();
+    return template;
+  }
+  
+  //Load, Grab out of the DOM and Render the header and footer
+  export async function loadHeaderFooter() {
+    // console.log("loadHeaderFooter");
+    // Load the header only if it has not been loaded before.
+    if (!headerLoaded) {
+      const headerTemplate = await loadTemplate("../headerFooter/header.html");
+      const headerElement = document.querySelector("#header");
+  
+      if (!headerElement.innerHTML.trim()) {
+        renderwithTemplate(headerTemplate, headerElement);
+      }
+      headerLoaded = true; // We mark that the header has already been loaded.
+    }
+    // Load the footer only if it has not been loaded before.
+    if (!footerLoaded) {
+      const footerTemplate = await loadTemplate("../headerFooter/footer.html");
+      const footerElement = document.querySelector("#footer");
+  
+      if (!footerElement.innerHTML.trim()) {
+        renderwithTemplate(footerTemplate, footerElement);
+      }
+      footerLoaded = true; // We mark that the footer has already been loaded.
+    }
+  }
